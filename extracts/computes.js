2dd0bd549f3aaceb279302ccf4d8f9d8b92e52be
Pierre CAVIN <tech@sherlox.io>
2020-04-09 01:21:52 +0200

    "decode" signal computations

--- extracts/computes.js
+++ extracts/computes.js
@@ -1,81 +1,79 @@
-{
-  computeMASignal = function (e, t) {
-    var o = n.NEUTRAL;
-    return e < t && (o = n.BUY),
-      e > t && (o = n.SELL),
-      o
+const movingAverages = {
+  computeMASignal: function (ma, close) {
+    if (ma < close) return n.BUY
+    if (ma > close) return n.SELL
+    return n.NEUTRAL
   },
-  computeRSISignal= function (e, t) {
-    var o = n.NEUTRAL;
-    return e < 30 && t > e && (o = n.BUY),
-      e > 70 && t < e && (o = n.SELL),
-      o
+}
+
+const oscillators = {
+  computeRSISignal: function (rsi7, prevRsi7) {
+    if (e < 30 && prevRsi7 > rsi7) return n.BUY
+    if (e > 70 && prevRsi7 < rsi7) return n.SELL
+    return n.NEUTRAL
   },
-  computeStochSignal= function (e, t, o, r) {
-    var i = n.NEUTRAL;
-    return e < 20 && t < 20 && e > t && o < r && (i = n.BUY),
-      e > 80 && t > 80 && e < t && o > r && (i = n.SELL),
-      i
+
+  computeStochSignal: function (stochK, stochD, prevStochK, prevStochD) {
+    if (stochK < 20 && stochD < 20 && stochK > stochD && prevStochK < prevStochD) return n.BUY
+    if (stochK > 80 && stochD > 80 && stochK < stochD && prevStochK > prevStochD) return n.SELL
+    return n.NEUTRAL
   },
-  computeCCI20Signal= function (e, t) {
-    var o = n.NEUTRAL;
-    return e < -100 && e > t && (o = n.BUY),
-      e > 100 && e < t && (o = n.SELL),
-      o
+
+  computeCCI20Signal: function (cci20, prevCci20) {
+    if (cci20 < -100 && cci20 > prevCci20) return n.BUY
+    if (cci20 > 100 && cci20 < prevCci20) return n.SELL
+    return n.NEUTRAL
   },
-  computeADXSignal= function (e, t, o, r, i) {
-    var a = n.NEUTRAL;
-    return e > 20 && r < i && t > o && (a = n.BUY),
-      e > 20 && r > i && t < o && (a = n.SELL),
-      a
+
+  computeADXSignal: function (adx, adxPlusDi, adxMinusDi, prevAdxPlusDi, prevAdxMinuxDi) {
+    if (adx > 20 && prevAdxPlusDi < prevAdxMinuxDi && adxPlusDi > adxMinusDi) return n.BUY
+    if (adx > 20 && prevAdxPlusDi > prevAdxMinuxDi && adxPlusDi < adxMinusDi) return n.SELL
+    return n.NEUTRAL
   },
-  computeAOSignal= function (e, t) {
-    var o = n.NEUTRAL;
-    return (e > 0 && t < 0 || e > 0 && t > 0 && e > t) && (o = n.BUY),
-      (e < 0 && t > 0 || e < 0 && t < 0 && e < t) && (o = n.SELL),
-      o
+
+  computeAOSignal: function (ao, prevAo) {
+    if (ao > 0 && prevAo < 0 || ao > 0 && prevAo > 0 && ao > prevAo) return n.BUY
+    if (ao < 0 && prevAo > 0 || ao < 0 && prevAo < 0 && ao < prevAo) return n.SELL
+    return n.NEUTRAL
   },
-  computeMomSignal= function (e, t) {
-    var o = n.NEUTRAL;
-    return e < t && (o = n.BUY),
-      e > t && (o = n.SELL),
-      o
+
+  computeMomSignal: function (mom, prevMom) {
+    if (mom < prevMom) return n.BUY
+    if (mom > prevMom) return n.SELL
+    return n.NEUTRAL
   },
-  computeMACDSignal= function (e, t) {
-    var o = n.NEUTRAL;
-    return e > t && (o = n.BUY),
-      e < t && (o = n.SELL),
-      o
+
+  computeMACDSignal: function (macd, macdSignal) {
+    if (macd > macdSignal) return n.BUY
+    if (macd < macdSignal) return n.SELL
+    return n.NEUTRAL
   },
-  computeBBBuySignal= function (e, t) {
-    var o = n.NEUTRAL;
-    return e < t && (o = n.BUY),
-      o
+
+  computeBBSignal: function (close, bbLower, bbUpper) {
+    if (close < bbLower) return n.BUY
+    if (close > bbUpper) return n.SELL
+    return n.NEUTRAL
   },
-  computeBBSellSignal= function (e, t) {
-    var o = n.NEUTRAL;
-    return e > t && (o = n.SELL),
-      o
+
+  computePSARSignal: function (psar, open) {
+    if (psar < open) return n.BUY
+    if (psar > open) return n.SELL
+    return n.NEUTRAL
+  }
+}
+
+const misc = {
+  computeSimpleSignal: function (signal) {
+    if (signal === 1) return n.BUY
+    if (signal === -1) return n.SELL
+    return n.NEUTRAL
   },
-  computePSARSignal= function (e, t) {
-    var o = n.NEUTRAL;
-    return e < t && (o = n.BUY),
-      e > t && (o = n.SELL),
-      o
-  },
-  computeRecommendSignal= function (e) {
-    var t = void 0;
-    return e >= -1 && e < -.5 && (t = n.STRONG_SELL),
-      e >= -.5 && e < 0 && (t = n.SELL),
-      0 === e && (t = n.NEUTRAL),
-      e > 0 && e <= .5 && (t = n.BUY),
-      e > .5 && e <= 1 && (t = n.STRONG_BUY),
-      t
-  },
-  computeSimpleSignal= function (e) {
-    var t = n.NEUTRAL;
-    return -1 === e && (t = n.SELL),
-      1 === e && (t = n.BUY),
-      t
+
+  computeRecommendSignal: function (e) {
+    if (e >= -1 && e < -.5) return n.STRONG_SELL
+    if (e >= -.5 && e < 0) return n.SELL
+    if (e === 0) return n.NEUTRAL
+    if (e > 0 && e <= .5) return n.BUY
+    if (e > .5 && e <= 1) return n.STRONG_BUY
   }
 }
